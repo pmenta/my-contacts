3c7f5537c08f01bdaf1425225bcd98c9f3c2dfee
João Vitor Martins Melo <https://joaoWe@bitbucket.org>
2022-04-21 15:16:57 -0300

    refactoring phone input to uncontrolled

--- src/components/ContactsForm/index.jsx
+++ src/components/ContactsForm/index.jsx
@@ -1,7 +1,5 @@
-// import { useRef } from 'react';
 import PropTypes from 'prop-types';
-import { useForm, Controller } from 'react-hook-form';
-import InputMask from 'react-input-mask';
+import { useForm } from 'react-hook-form';
 
 import { FormGroup } from '../FormGroup';
 import { Input } from '../Input';
@@ -9,18 +7,18 @@ import { Select } from '../Select';
 import { Button } from '../Button';
 
 import { Form } from './styles';
+import { formatPhone } from '../../utils/formatPhone';
 
 export function ContactForm({ buttonLabel }) {
   const {
-    register, handleSubmit, formState: { errors }, control,
+    register, handleSubmit, formState: { errors }, setValue,
   } = useForm();
 
   function onSubmit(data) {
     console.log(data);
   }
 
-  // const maskRef = useRef(null);
-  // console.log(getValues('phone'));
+  const phoneRegex = /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})-?(\d{4}))$/;
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)} noValidate>
@@ -36,40 +34,24 @@ export function ContactForm({ buttonLabel }) {
       </FormGroup>
       <FormGroup error={errors.email ? 'Insira um e-mail válido' : ''}>
         <Input
-          {...register('email', { required: true, mode: 'onChange', pattern: /^\S+@\S+\.\S+$/ })}
+          {...register('email', { required: false, pattern: /^\S+@\S+\.\S+$/ })}
           error={errors.email}
           type="email"
           placeholder="Email"
         />
       </FormGroup>
       <FormGroup error={errors.phone ? 'Insira um telefone válido' : ''}>
-        <Controller
-          name="phone"
-          control={control}
-          rules={{ required: true, minLength: 16 }}
-          defaultValue=""
-          render={({ field }) => (
-            <InputMask
-              {...field}
-              mask="(99) 9 9999-9999"
-              maskChar=""
-              alwaysShowMask={false}
-              type="phone"
-              placeholder="Telefone"
-              error={errors.phone}
-            >
-              {(inputProps) => (
-                <Input
-                  {...inputProps}
-                />
-              )}
-            </InputMask>
-          )}
+        <Input
+          {...register('phone', { required: false, mode: 'onChange', pattern: phoneRegex })}
+          error={errors.phone}
+          type="phone"
+          placeholder="Telefone"
+          onChange={(ev) => setValue('phone', formatPhone(ev.currentTarget.value), { shouldValidate: true })}
         />
       </FormGroup>
       <FormGroup error={errors.category?.type === 'required' ? 'Selecione uma categoria' : ''}>
         <Select
-          {...register('category', { required: true })}
+          {...register('category', { required: false })}
           error={errors.category}
         >
           <option value="">Selecione uma categoria</option>
